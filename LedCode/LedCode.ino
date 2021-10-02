1e2404076408f9bcf431a0e4ffc4e691d29e283b
MRI6HFB <MRI6HFB@virginia.edu>
2021-10-02 12:31:25 -0400

    Added better pin definitions

--- LedCode/LedCode.ino
+++ LedCode/LedCode.ino
@@ -1,4 +1,6 @@
 #define LED_PIN 5
+#define LED_TYPE WS2813
+#define NUM_SEGMENT_LEDS 14
 
 #include <FastLED.h>
 
@@ -7,11 +9,11 @@ CRGB leds[100];
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
-  FastLED.addLeds<WS2813, LED_PIN>(leds, 1);
+  FastLED.addLeds<LED_TYPE, LED_PIN>(leds, NUM_SEGMENT_LEDS);
 }
 
 void loop() {
-  for(int i = 0; i < 100; i++) {
+  for(int i = 0; i < NUM_SEGMENT_LEDS; i++) {
     leds[i] = CHSV(120, 120, 120);
   }
   FastLED.show();
